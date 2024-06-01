d1960d85bdde164c087bd64c2bdf3ddf677bd2ad
bubbles-wow <917749218@qq.com>
2024-06-01 21:58:47 +0800

    add timeout for refresh token

--- refresh_tokens.py
+++ refresh_tokens.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import time
 import gzip
 import json
@@ -166,27 +167,36 @@ else:
         }
     }
 
-access_token, last_used, expires = Xbox.get_access_token(path)
-tokens["Xbox"]["AccessToken"]["Jwt"] = access_token
-tokens["Xbox"]["AccessToken"]["LastUsed"] = last_used.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-tokens["Xbox"]["AccessToken"]["Expires"] = expires.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+def updater():
+    access_token, last_used, expires = Xbox.get_access_token(path)
+    tokens["Xbox"]["AccessToken"]["Jwt"] = access_token
+    tokens["Xbox"]["AccessToken"]["LastUsed"] = last_used.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    tokens["Xbox"]["AccessToken"]["Expires"] = expires.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
-user_token, last_used, expires = Xbox.get_user_token(access_token)
-tokens["Xbox"]["UserToken"]["Jwt"] = user_token
-tokens["Xbox"]["UserToken"]["LastUsed"] = last_used.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-tokens["Xbox"]["UserToken"]["Expires"] = expires.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    user_token, last_used, expires = Xbox.get_user_token(access_token)
+    tokens["Xbox"]["UserToken"]["Jwt"] = user_token
+    tokens["Xbox"]["UserToken"]["LastUsed"] = last_used.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    tokens["Xbox"]["UserToken"]["Expires"] = expires.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
-x_token, user_hashcode, last_used, expires = Xbox.get_x_token(user_token)
-tokens["Xbox"]["XToken"]["Jwt"] = x_token
-tokens["Xbox"]["XToken"]["LastUsed"] = last_used.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-tokens["Xbox"]["XToken"]["Expires"] = expires.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-tokens["Xbox"]["UserHashCode"] = user_hashcode
+    x_token, user_hashcode, last_used, expires = Xbox.get_x_token(user_token)
+    tokens["Xbox"]["XToken"]["Jwt"] = x_token
+    tokens["Xbox"]["XToken"]["LastUsed"] = last_used.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    tokens["Xbox"]["XToken"]["Expires"] = expires.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    tokens["Xbox"]["UserHashCode"] = user_hashcode
 
-last_used_time, expires_time, token = Store.get_token(path)
-tokens["Store"]["AccessToken"]["Jwt"] = token
-tokens["Store"]["AccessToken"]["LastUsed"] = last_used_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-tokens["Store"]["AccessToken"]["Expires"] = expires_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    last_used_time, expires_time, token = Store.get_token(path)
+    tokens["Store"]["AccessToken"]["Jwt"] = token
+    tokens["Store"]["AccessToken"]["LastUsed"] = last_used_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    tokens["Store"]["AccessToken"]["Expires"] = expires_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
-with open("tokens.json", "w") as f:
-    f.write(json.dumps(tokens, indent=4))
-    f.close()
+    with open("tokens.json", "w") as f:
+        f.write(json.dumps(tokens, indent=4))
+        f.close()
+
+def __main__():
+    thread = threading.Thread(target=updater)
+    thread.start()
+    thread.join(120)
+    if thread.is_alive():
+        thread._stop()
+        print("Timeout")
